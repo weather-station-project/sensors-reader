65a311db15a36d576042afea8f85ebe8b95f0ad5
David <david.leon@nexthink.com>
2020-11-18 00:49:19 +0100

    jenkins files

---
+++ Jenkinsfile_master.groovy
@@ -0,0 +1,90 @@
+@Library('shared-library') _
+import com.davidleonm.WeatherStationSensorsReaderVariables
+
+pipeline {
+  agent { label 'slave' }
+
+  stages {
+    stage('Prepare Python ENV') {
+      steps {
+        script {
+          setBuildStatus('pending', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+
+          // Clean & Prepare new python environment
+          sh 'rm -rf ENV'
+          sh 'python3 -m venv ENV'
+
+          sh 'ENV/bin/pip install --upgrade pip'
+          sh "ENV/bin/pip install -r ${WORKSPACE}/WeatherStationSensorsReader/requirements.txt"
+        }
+      }
+    }
+
+    stage('Execute unit tests') {
+      steps {
+        script {
+          sh "ENV/bin/python -m unittest discover -s ${WORKSPACE}/WeatherStationSensorsReader"
+        }
+      }
+    }
+
+    stage('SonarQube analysis') {
+      environment {
+        def scannerHome = tool 'Sonarqube'
+      }
+
+      steps {
+        script {
+          sh "ENV/bin/coverage run -m unittest discover -s ${WORKSPACE}/WeatherStationSensorsReader"
+          sh "ENV/bin/coverage xml -i"
+        }
+
+        withSonarQubeEnv('Sonarqube') {
+          sh "${scannerHome}/bin/sonar-scanner"
+        }
+
+        timeout(time: 10, unit: 'MINUTES') {
+          waitForQualityGate abortPipeline: true
+        }
+      }
+    }
+
+    stage('Build & Deploy image') {
+      steps {
+        script {
+          def dockerImage = null
+
+          try {
+            version = sh(script: 'cat VERSION', returnStdout: true)
+            dockerImage = docker.build("${WeatherStationSensorsReaderVariables.DockerHubRegistryName}", "--file ./Dockerfile ${WORKSPACE}")
+
+            docker.withRegistry('', 'docker-hub-login') {
+              dockerImage.push("${version}")
+              dockerImage.push('latest')
+            }
+          } finally {
+            if (dockerImage != null) {
+              sh """
+                docker rmi -f ${WeatherStationSensorsReaderVariables.DockerHubRegistryName}:${version}
+                docker rmi -f ${WeatherStationSensorsReaderVariables.DockerHubRegistryName}:latest
+              """
+            }
+          }
+        }
+      }
+    }
+  }
+  post {
+    success {
+      script {
+        setBuildStatus('success', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+
+    failure {
+      script {
+        setBuildStatus('failure', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+  }
+}
---
+++ Jenkinsfile_multi_branch.groovy
@@ -0,0 +1,44 @@
+@Library('shared-library') _
+import com.davidleonm.WeatherStationSensorsReaderVariables
+
+pipeline {
+  agent { label 'slave' }
+
+  stages {
+    stage('Prepare Python ENV') {
+      steps {
+        script {
+          setBuildStatus('pending', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+
+          // Clean & Prepare new python environment
+          sh 'rm -rf ENV'
+          sh 'python3 -m venv ENV'
+
+          sh 'ENV/bin/pip install --upgrade pip'
+          sh "ENV/bin/pip install -r ${WORKSPACE}/WeatherStationSensorsReader/requirements.txt"
+        }
+      }
+    }
+
+    stage('Execute unit tests') {
+      steps {
+        script {
+          sh "ENV/bin/python -m unittest discover -s ${WORKSPACE}/WeatherStationSensorsReader"
+        }
+      }
+    }
+  }
+  post {
+    success {
+      script {
+        setBuildStatus('success', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+
+    failure {
+      script {
+        setBuildStatus('failure', "${WeatherStationSensorsReaderVariables.RepositoryName}")
+      }
+    }
+  }
+}
