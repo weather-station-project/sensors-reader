ae9336622f71b9f08245a19e2cef591cda1079a0
David Leon <david.leon@nexthink.com>
2021-03-17 22:07:57 +0100

    finishing unit tests

--- WeatherStationSensorsReader/tests/sensors/test_fake_sensor.py
+++ WeatherStationSensorsReader/tests/sensors/test_fake_sensor.py
@@ -4,10 +4,10 @@ from sensors.fake_sensor import FakeSensor
 
 
 class TestFakeSensor(unittest.TestCase):
-    def test_when_reading_values_random_number_should_be_returned(self):
+    def test_when_getting_readings_random_number_should_be_returned(self):
         fake_sensor = FakeSensor()
 
-        result = fake_sensor.read_values()
+        result = fake_sensor.get_reading()
 
         self.assertIsInstance(result, list)
         self.assertEqual(len(result), fake_sensor.VALUES_NUMBER)
--- WeatherStationSensorsReader/tests/sensors/test_wind_measurement_sensor.py
+++ WeatherStationSensorsReader/tests/sensors/test_wind_measurement_sensor.py
@@ -8,59 +8,38 @@ from sensors.wind_measurement_sensor import WindMeasurementSensor
 class TestWindMeasurementSensor(unittest.TestCase):
     test_port_number = 55
 
+    @mock.patch('sensors.wind_measurement_sensor.Sensor.__init__')
     @mock.patch('sensors.wind_measurement_sensor.Anemometer')
     @mock.patch('sensors.wind_measurement_sensor.Vane')
-    def test_when_calling_constructor_new_mock_should_be_initialized(self, mock_vane, mock_anemometer):
-        self.assertIsNotNone(WindMeasurementSensor(anemometer_port_number=self.test_port_number))
-
-        mock_anemometer.assert_called_once_with(anemometer_port_number=self.test_port_number)
-        mock_vane.assert_called_once()
-
-    @mock.patch('sensors.wind_measurement_sensor.Anemometer')
-    @mock.patch('sensors.wind_measurement_sensor.Vane')
-    def test_when_reading_values_given_some_samples_expected_values_should_be_returned(self, mock_vane, mock_anemometer):
-        # arrange
-        test_sample = 10
-        test_direction_angle = 100
-
-        mock_anemometer_device = Mock()
-        mock_anemometer_device.get_sample.return_value = test_sample
-        mock_anemometer.return_value = mock_anemometer_device
-
+    def setUp(self, mock_vane, mock_anemometer, mock_super):
         mock_vane_device = Mock()
-        mock_vane_device.get_sample.return_value = test_direction_angle
         mock_vane.return_value = mock_vane_device
 
-        # act
-        sensor = WindMeasurementSensor(anemometer_port_number=self.test_port_number)
-        self.assertEqual(sensor.read_values(), [test_direction_angle, test_sample])
+        self.test_sensor = WindMeasurementSensor(anemometer_port_number=self.test_port_number)
 
-        # assert
         mock_anemometer.assert_called_once_with(anemometer_port_number=self.test_port_number)
         mock_vane.assert_called_once()
-        mock_anemometer_device.get_sample.assert_called_once()
-        mock_vane_device.get_sample.assert_called_once()
+        mock_super.assert_called_once()
 
-    @mock.patch('sensors.wind_measurement_sensor.Anemometer')
-    @mock.patch('sensors.wind_measurement_sensor.Vane')
-    def test_when_getting_averages_expected_calls_should_be_done_and_expected_values_returned(self, mock_vane, mock_anemometer):
+    def test_when_getting_readings_given_some_samples_expected_values_should_be_returned(self):
         # arrange
-        test_reads = [[10, 1],
-                      [15, 5],
-                      [15, 2],
-                      [30, 1],
-                      [30, 1]]
-        test_direction_average = 'N-NW'
-        test_expected_values = [test_direction_average, 2, 5]
-
-        mock_vane.return_value.get_direction_average = MagicMock(return_value=test_direction_average)
+        test_direction_angle = 100
+        self.test_sensor.vane.get_reading.return_value = test_direction_angle
 
         # act
-        sensor = WindMeasurementSensor(anemometer_port_number=self.test_port_number)
-        self.assertEqual(sensor.get_average(reads=test_reads), test_expected_values)
+        self.assertEqual(self.test_sensor.get_reading(), [test_direction_angle])
 
+    def test_when_getting_averages_expected_calls_should_be_done_and_expected_values_returned(self):
         # arrange
-        mock_vane.return_value.get_direction_average.assert_called_once_with(direction_angles=(10, 15, 15, 30, 30))
+        test_speed = 58
+        test_direction_average = 'N-NW'
+
+        self.test_sensor.anemometer.get_speed = MagicMock(return_value=test_speed)
+        self.test_sensor.vane.get_direction_average = MagicMock(return_value=test_direction_average)
+
+        # act
+        self.test_sensor.readings = [[10], [15], [15], [30], [30]]
+        self.assertEqual(self.test_sensor.get_average(), [test_direction_average, test_speed])
 
 
 if __name__ == '__main__':
