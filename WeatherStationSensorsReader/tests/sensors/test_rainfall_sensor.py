d19994e6a623d51d964056942d6e38964d0980e1
David Leon <david.leon@nexthink.com>
2021-04-02 18:16:25 +0200

    UT

---
+++ WeatherStationSensorsReader/tests/sensors/test_rainfall_sensor.py
@@ -0,0 +1,34 @@
+import unittest
+from unittest import mock
+
+from sensors.rainfall_sensor import RainfallSensor
+
+
+class TestRainfallSensor(unittest.TestCase):
+    @mock.patch('sensors.rainfall_sensor.Sensor.__init__')
+    @mock.patch('sensors.rainfall_sensor.logging')
+    @mock.patch('sensors.rainfall_sensor.Button')
+    def setUp(self, mock_button, mock_logging, mock_super):
+        test_rain_gauge_port_number = 51
+
+        self.test_sensor = RainfallSensor(rain_gauge_port_number=test_rain_gauge_port_number)
+
+        self.assertIsNotNone(self.test_sensor)
+        mock_button.assert_called_once()
+        mock_logging.debug.assert_called_once_with(msg=f'[RainfallSensor] Started on the port "{test_rain_gauge_port_number}".')
+        mock_super.assert_called_once()
+
+    def test_when_adding_values_nothing_should_be_returned(self):
+        self.assertIsNone(self.test_sensor.add_value_to_readings())
+
+    def test_when_getting_readings_1_should_be_returned(self):
+        self.assertEqual(self.test_sensor.get_reading(), [1])
+
+    def test_when_getting_averages_given_1_reading_expected_result_should_be_returned(self):
+        self.test_sensor.readings = [1]
+
+        self.assertEqual(self.test_sensor.get_average(), self.test_sensor.BUCKET_SIZE_IN_MM)
+
+
+if __name__ == '__main__':
+    unittest.main()
